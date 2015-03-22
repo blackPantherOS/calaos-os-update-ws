8cf1e614083c6c69368c1b43e9149544c8b60e6d
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2015-03-22 16:38:25 +0100

    Add script for creation of current versions

---
+++ current_versions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+node add_version.js raspberrypi v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/raspberrypi/calaos-image-raspberrypi-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js cubieboard2 v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/cubieboard2/calaos-image-cubieboard2-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js meleg v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/meleg/calaos-image-meleg-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js nuc v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/nuc/calaos-image-nuc-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js mele v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/mele/calaos-image-mele-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js cubieboard v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/cubieboard/calaos-image-cubieboard-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js n450 v2.0-rc2-0-g4eb85a1 testing http://calaos.fr/download/testing/calaos-os/n450/calaos-image-n450-v2.0-rc2-0-g4eb85a1-2015-01-06.tar.xz
+node add_version.js raspberrypi v2.0 stable http://calaos.fr/download/stable/calaos-os/raspberrypi/calaos-image-raspberrypi-v2.0.tar.xz
+node add_version.js cubieboard2 v2.0 stable http://calaos.fr/download/stable/calaos-os/cubieboard2/calaos-image-cubieboard2-v2.0.tar.xz
+node add_version.js meleg v2.0 stable http://calaos.fr/download/stable/calaos-os/meleg/calaos-image-meleg-v2.0.tar.xz
+node add_version.js nuc v2.0 stable http://calaos.fr/download/stable/calaos-os/nuc/calaos-image-nuc-v2.0.tar.xz
+node add_version.js mele v2.0 stable http://calaos.fr/download/stable/calaos-os/mele/calaos-image-mele-v2.0.tar.xz
+node add_version.js cubieboard v2.0 stable http://calaos.fr/download/stable/calaos-os/cubieboard/calaos-image-cubieboard-v2.0.tar.xz
+node add_version.js n450 v2.0 stable http://calaos.fr/download/stable/calaos-os/n450/calaos-image-n450-v2.0.tar.xz
